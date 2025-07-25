d59c8defbe1b3b5773fac9f027e5039ba71d491a
Emmanuel Diniz <emmanuel.dinizc1@gmail.com>
2025-07-25 01:35:18 -0300

    feat: tipoconta

--- src/main/java/com/cefet/trab_republica/entities/TipoConta.java
+++ src/main/java/com/cefet/trab_republica/entities/TipoConta.java
@@ -1,42 +1,71 @@
 package com.cefet.trab_republica.entities;
 
 import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
-@Table(name = "tb_tipo_conta")
-public class TipoConta {
+@Table(name = "tb_tipoconta")
+public class TipoConta implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private String descricao;
 
-    @Column(length = 500)
+    @Column(length = 255)
     private String observacao;
+    
+    // Construtores, Getters, Setters, hashCode e equals (sem alterações)
+    public TipoConta() {}
 
-	public Long getId() {
-		return id;
-	}
+    public TipoConta(Long id, String descricao, String observacao) {
+        this.id = id;
+        this.descricao = descricao;
+        this.observacao = observacao;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getDescricao() {
-		return descricao;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    public String getDescricao() {
+        return descricao;
+    }
 
-	public String getObservacao() {
-		return observacao;
-	}
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
-	public void setObservacao(String observacao) {
-		this.observacao = observacao;
-	}
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TipoConta other = (TipoConta) obj;
+        return Objects.equals(id, other.id);
+    }
 }
