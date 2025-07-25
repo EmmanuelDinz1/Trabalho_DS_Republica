3bde62fe485f3751a64dadbe731c48faabb21fa8
Emmanuel Diniz <emmanuel.dinizc1@gmail.com>
2025-07-25 01:50:05 -0300

    feat: rateio correcao

--- src/main/java/com/cefet/trab_republica/entities/Rateio.java
+++ src/main/java/com/cefet/trab_republica/entities/Rateio.java
@@ -1,65 +1,66 @@
 package com.cefet.trab_republica.entities;
 
 import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
+// --- CORREÇÃO 1: Especificar o nome da tabela ---
 @Table(name = "tb_rateio")
-public class Rateio {
+public class Rateio implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "conta_id")
-    private Conta conta;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "morador_id")
-    private Morador morador;
-
     @Column(nullable = false)
-    private double valor;
+    private Double valor;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private StatusRateio status;
 
-    // Getters / Setters
+    @ManyToOne
+    @JoinColumn(name = "id_conta")
+    private Conta conta;
 
-    public Long getId() {
-        return id;
-    }
+    @ManyToOne
+    @JoinColumn(name = "id_morador")
+    private Morador morador;
 
-    public Conta getConta() {
-        return conta;
-    }
+    // Construtores, Getters, Setters, etc. (sem alterações)
+    public Rateio() {}
 
-    public void setConta(Conta conta) {
+    public Rateio(Long id, Double valor, StatusRateio status, Conta conta, Morador morador) {
+        this.id = id;
+        this.valor = valor;
+        this.status = status;
         this.conta = conta;
-    }
-
-    public Morador getMorador() {
-        return morador;
-    }
-
-    public void setMorador(Morador morador) {
         this.morador = morador;
     }
 
-    public double getValor() {
-        return valor;
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Double getValor() { return valor; }
+    public void setValor(Double valor) { this.valor = valor; }
+    public StatusRateio getStatus() { return status; }
+    public void setStatus(StatusRateio status) { this.status = status; }
+    public Conta getConta() { return conta; }
+    public void setConta(Conta conta) { this.conta = conta; }
+    public Morador getMorador() { return morador; }
+    public void setMorador(Morador morador) { this.morador = morador; }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public StatusRateio getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusRateio status) {
-        this.status = status;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Rateio rateio = (Rateio) obj;
+        return Objects.equals(id, rateio.id);
     }
 }
